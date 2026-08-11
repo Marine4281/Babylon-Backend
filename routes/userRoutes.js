f4743ecd9fd71638af57bb1ae63413f5e64e6d15
Marine4281 <marinepro360@gmail.com>
2026-08-11 22:20:30 +0300

    Add avatar and cover photo upload routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { getProfile, updateProfile, toggleFollow } from "../controllers/userController.js";
+import { getProfile, updateProfile, toggleFollow, uploadAvatar, uploadCoverPhoto } from "../controllers/userController.js";
 import { protect } from "../Middlewares/authMiddleware.js";
+import { uploadAvatar as uploadAvatarMiddleware, uploadCoverPhoto as uploadCoverPhotoMiddleware } from "../Config/cloudinary.js";
 
 const router = express.Router();
 
 router.use(protect);
 
 router.put("/me", updateProfile);
+router.post("/me/avatar", uploadAvatarMiddleware.single("avatar"), uploadAvatar);
+router.post("/me/cover-photo", uploadCoverPhotoMiddleware.single("cover"), uploadCoverPhoto);
 router.get("/:username", getProfile);
 router.post("/:id/follow", toggleFollow);
 
