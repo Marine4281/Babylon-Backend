cb830c9efab946143443d675071450d5eb3623d6
Marine4281 <marinepro360@gmail.com>
2026-08-11 19:59:20 +0300

    Add video and audio room routes

--- routes/liveRoutes.js
+++ routes/liveRoutes.js
@@ -1,23 +1,38 @@
 import express from "express";
 import {
-  startLiveRoom,
   joinLiveRoom,
   leaveLiveRoom,
   endLiveRoom,
   getLiveRooms,
   giftLiveRoom,
 } from "../controllers/liveController.js";
+import { startVideoLiveRoom } from "../controllers/liveVideoController.js";
+import {
+  startAudioLiveRoom,
+  requestToSpeak,
+  approveSpeaker,
+  removeSpeaker,
+} from "../controllers/audioLiveController.js";
 import { protect } from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.use(protect);
 
+// Shared
 router.get("/", getLiveRooms);
-router.post("/", startLiveRoom);
 router.post("/:roomId/join", joinLiveRoom);
 router.post("/:roomId/leave", leaveLiveRoom);
 router.post("/:roomId/end", endLiveRoom);
 router.post("/:roomId/gift", giftLiveRoom);
+
+// Video-only
+router.post("/video", startVideoLiveRoom);
+
+// Voice-only
+router.post("/voice", startAudioLiveRoom);
+router.post("/voice/:roomId/request-speak", requestToSpeak);
+router.post("/voice/:roomId/approve-speak/:userId", approveSpeaker);
+router.post("/voice/:roomId/remove-speaker/:userId", removeSpeaker);
 
 export default router;
