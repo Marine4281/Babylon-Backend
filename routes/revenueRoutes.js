a1b3abee6ea2fbf51cdf5e1fa88b648cf22a9089
Marine4281 <marinepro360@gmail.com>
2026-08-11 16:06:05 +0300

    Delete routes/revenueRoutes.js

--- routes/revenueRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/revenueRoutes.js
-import express from "express";
-import { getTodayRevenue } from "../controllers/revenueController.js";
-
-const router = express.Router();
-
-router.get("/today", getTodayRevenue);
-
-export default router;
