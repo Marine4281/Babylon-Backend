27aa57bcb818233640b5a3913caf293695c0826a
Marine4281 <marinepro360@gmail.com>
2026-08-11 22:19:18 +0300

    Add upload media route to postRoutes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
-import { createPost, getFeed, toggleLike, toggleSave } from "../controllers/postController.js";
+import { createPost, getFeed, toggleLike, toggleSave, uploadPostMedia } from "../controllers/postController.js";
 import { getComments, addComment, addGiftComment } from "../controllers/commentController.js";
 import { protect } from "../Middlewares/authMiddleware.js";
+import { uploadPostMedia as uploadPostMediaMiddleware } from "../Config/cloudinary.js";
 
 const router = express.Router();
 
 router.use(protect);
 
 router.get("/", getFeed);
+router.post("/upload-media", uploadPostMediaMiddleware.array("media", 10), uploadPostMedia);
 router.post("/", createPost);
 router.post("/:id/like", toggleLike);
 router.post("/:id/save", toggleSave);
